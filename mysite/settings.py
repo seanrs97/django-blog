4154ec1e6d09cc23b570e07021d9c037838e850b
seanrs <sean@incuna.com>
2017-08-14 11:53:56 +0100

    settings.py update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y3rq6f8&23hgumkn2k6)=js^z$3g8c4gz)f1ju2y^hj=ju6ti&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['seanrs97.pythonanywhere.com']
 
 
 # Application definition
